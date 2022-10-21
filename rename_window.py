b4bf234650c3247698ad0afa79c65f17ff761aac
chenmy1903 <chenmy10@outlook.com>
2022-10-21 22:30:19 +0800

    傻逼乔博远你妈死了LL别只会圈人想想别人为什么不进你工会而来我们AFL

--- rename_window.py
+++ rename_window.py
@@ -18,7 +18,15 @@ motds = [
     "qby love hyc<3",
     "诶哟~嘿~",
     "QiaoBoYuanL died :(",
+    "小黑板你得改密码啊,你号都被盗了",
+    "小黑板树脂666我们都叫他乔博远,就他叫他小黑板",
+    "傻逼乔博远想想别人为什么不去你工会而来我们AFL",
+    "Title changed by rename_window.py!",
+    "[Loser XiaoHeiPa] 差点忘了自己是来打飞机的了",
+    "Buy iqbooster?",
     "ILY<3",
+    "github.com/XiaoHeiPa - 乔博远Official GitHub!",
+    "Sub to 兰方翔Official on bilibili!",
     "huanmeng_LLL: You are banned.",
 ] + (["Pls a rank because today is 11/7!"] if (time_now.tm_mday == 7 and time_now.tm_mon == 11) else [])
 
@@ -37,7 +45,7 @@ def set_title():
             if old_title == "":
                 old_title = title
             motd = random.choice(motds)
-            win32api.SendMessage(window,win32con.WM_SETTEXT,0,"Lunar Client CN" + old_title.replace("Lunar Client", "") + " | " + motd)
+            win32api.SendMessage(window, win32con.WM_SETTEXT,0,"Lunar Client CN" + old_title.replace("Lunar Client", "") + " | " + motd)
             return True
     if not inited:
         sys.exit()
